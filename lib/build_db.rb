df7a0adf70b3c4845da7776b98c9fd884a750575
Peter Wilmott <p@p8952.info>
2013-08-07 23:08:08 +0100

    Refactor database generation script

--- lib/build_db.rb
+++ lib/build_db.rb
@@ -4,19 +4,20 @@ require 'jbuilder'
 require 'redis'
 require 'uri'
 
-if ENV["REDISCLOUD_URL"]
-  uri = URI.parse(ENV["REDISCLOUD_URL"])
-  @redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-else
-  @redis = Redis.new(:host => "127.0.0.1", :port => 6379)
+def setup_redis()
+  if ENV["REDISCLOUD_URL"]
+    uri = URI.parse(ENV["REDISCLOUD_URL"])
+    @redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  else  
+    @redis = Redis.new(:host => "127.0.0.1", :port => 6379)
+  end
+  @redis.keys.each { |key| @redis.del(key) }
 end
-@redis.keys.each { |key| @redis.del(key) }
 
-Dir.glob("#{File.expand_path(File.dirname(__FILE__))}/ebuilds/**/*-*.ebuild") do |ebuild|
-
+def parse_ebuild(ebuild)
   f = File.open(ebuild)
-  f_ebuild = f.read()
-  f.close()
+  f_ebuild = f.read
+  f.close
 
   e_category = File.basename(File.dirname(File.dirname(ebuild)))
   e_packagename = File.basename(ebuild, '.ebuild').sub(/-(?<=-)\d(.*)/, '')
@@ -26,27 +27,32 @@ Dir.glob("#{File.expand_path(File.dirname(__FILE__))}/ebuilds/**/*-*.ebuild") do
   e_homepage = f_ebuild.match(/(?<=HOMEPAGE=")(.*)(?=")/).to_s
   e_license = f_ebuild.match(/(?<=LICENSE=")(.*)(?=")/).to_s
 
-  unless e_keywords.nil? or e_keywords.empty? or
-         e_description.nil? or e_description.empty? or
-         e_homepage.nil? or e_homepage.empty? or
-         e_license.nil? or e_license.empty?
-  
-    e_json = Jbuilder.encode do |json|
-      json.set!(e_version) do
-        json.keywords(e_keywords)
-        json.description(e_description)
-        json.homepage(e_homepage)
-        json.license(e_license)
-      end
-    end
- 
-    puts "#{e_category}/#{e_packagename}-#{e_version}"
-    if @redis.exists("#{e_category}/#{e_packagename}")
-      @redis.set("#{e_category}/#{e_packagename}", (@redis.get("#{e_category}/#{e_packagename}").sub(/}$/,',') + e_json.sub(/^{/,'')))
-    else
-      @redis.set("#{e_category}/#{e_packagename}", e_json)
-    end
-
+  var_array = [e_keywords, e_description, e_homepage, e_license]
+  var_array.each do |var|
+    return nil, nil if var.nil? or var.empty?
   end
+  
+  e_json = Jbuilder.encode do |json|
+    json.set!(e_version) do
+      json.keywords(e_keywords)
+      json.description(e_description)
+      json.homepage(e_homepage)
+      json.license(e_license)
+    end
+  end
+  key = "#{e_category}/#{e_packagename}"
+  return key, e_json
+end
 
+setup_redis()
+Dir.glob("#{File.expand_path(File.dirname(__FILE__))}/ebuilds/**/*-*.ebuild") do |ebuild|
+  key, json = parse_ebuild(ebuild)
+  unless key.nil? or json.nil?
+    puts key
+    if @redis.exists(key)
+      @redis.set(key, (@redis.get(key).sub(/}$/,',') + json.sub(/^{/,'')))
+    else
+      @redis.set(key, json)
+    end
+  end
 end
